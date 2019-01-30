3618ee450816005ef3ffb63af0a1c47b60528869
dhelvik <daniel.helvik.93@gmail.com>
2019-01-30 12:46:24 +0100

    Revert "Update index.js"
    This reverts commit d1955e33878c5412e59da7b88a8d0338abc215d7.

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var http = require('http');
-var db = require('demo_db_connection');
 
 
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-    db.connect();
+    
 
 });
 
