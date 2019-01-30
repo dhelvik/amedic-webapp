4e4b12c0862ae79b41a050e9224a736bdb14c5c4
dhelvik <daniel.helvik.93@gmail.com>
2019-01-30 12:49:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var server = http.createServer(function(request, response) {
    /* response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
     */
-    res.redirect('demo_db_connection.js');
+    res.redirect('/demo_db_connection.js');
 });
 
 var port = process.env.PORT || 1337;
