6e06e00dde6d96536990252855fe64f6856f0a95
dhelvik <daniel.helvik.93@gmail.com>
2019-01-30 12:26:15 +0100

    Create demo_db_connection.js

---
+++ demo_db_connection.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "amedic-mysqldbserver.mysql.database.azure.com",
+  user: "mysqldbuser@amedic-mysqldbserver",
+  password: "Grupp2122"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
