243f49f2def15b5cc937bf862c4abbf4d5b26603
dhelvik <daniel.helvik.93@gmail.com>
2019-01-30 12:40:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
-    db.createConnection();
+    response.end(db.createConnection());
 
 
 });
