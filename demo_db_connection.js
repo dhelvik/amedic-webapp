2171d0a560168ea63cdbe73d9886db5632bbc7a7
dhelvik <daniel.helvik.93@gmail.com>
2019-01-30 13:38:44 +0100

    Update demo_db_connection.js

--- demo_db_connection.js
+++ demo_db_connection.js
@@ -1,6 +1,11 @@
 var http = require('http');
 var mysql = require('mysql');
-    var con = mysql.createConnection({
+
+
+var server = http.createServer(function (request, response) {
+
+
+       var con = mysql.createConnection({
         host: "amedic-mysqldbserver.mysql.database.azure.com",
         user: "mysqldbuser@amedic-mysqldbserver",
         password: "Grupp2122",
@@ -16,3 +21,5 @@ var mysql = require('mysql');
         });
     });
     
+});
+
