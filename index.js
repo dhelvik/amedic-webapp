4505513f1d32a0cf7a44de6242e850e76ebab692
dhelvik <daniel.helvik.93@gmail.com>
2019-01-30 12:39:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var http = require('http');
+var db = require('demo_db_connection');
+
 
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World!");
+    db.createConnection();
+
 
 });
 
