cebafd154f0b6d0d28f81070853f8a3ea514b404
MjauKatten <35685806+Smeeken@users.noreply.github.com>
2019-02-05 12:41:21 +0100

    connect och createcon

--- connect.js
+++ connect.js
@@ -1,10 +1,9 @@
 var mysql = require('mysql');
 
-con = createConnection({
+var con = mysql.createConnection({
         host: "amedic-mysqldbserver.mysql.database.azure.com",
         user: "mysqldbuser@amedic-mysqldbserver",
         password: "Grupp2122",
         database: "amedicdb"
-    })
-}
+    });
 module.exports = con;
