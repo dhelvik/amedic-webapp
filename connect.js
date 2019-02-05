27fdc92cf39cc951a9a84543b9d754c991c2fd92
MjauKatten <35685806+Smeeken@users.noreply.github.com>
2019-02-05 14:21:19 +0100

    mysql sequelize

--- connect.js
+++ connect.js
@@ -1,4 +1,7 @@
+const Sequelize = require('sequelize');
+
 var mysql = require('mysql');
+
 
 var con = mysql.createConnection({
         host: "amedic-mysqldbserver.mysql.database.azure.com",
@@ -6,4 +9,18 @@ var con = mysql.createConnection({
         password: "Grupp2122",
         database: "amedicdb"
     });
+/*
+const db = new Sequelize('amedicdb', 'mysqldbuser@amedic-mysqldbserver', 'Grupp2122', {
+    host: "amedic-mysqldbserver.mysql.database.azure.com",
+    dialect: "mysql",
+    pool:{
+        min:0,
+        acquire: 30000,
+        idle: 100000
+    }
+
+});
+*/
+
 module.exports = con;
+//module.exports = db;
