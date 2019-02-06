53dc730e0fe6df67305cf256595a8fbec64efb66
MjauKatten <35685806+Smeeken@users.noreply.github.com>
2019-02-06 14:40:50 +0100

    operatorAliases

--- connect.js
+++ connect.js
@@ -1,5 +1,44 @@
 const Sequelize = require('sequelize');
 
+const Op = Sequelize.Op;
+const operatorsAliases = {
+    $eq: Op.eq,
+    $ne: Op.ne,
+    $gte: Op.gte,
+    $gt: Op.gt,
+    $lte: Op.lte,
+    $lt: Op.lt,
+    $not: Op.not,
+    $in: Op.in,
+    $notIn: Op.notIn,
+    $is: Op.is,
+    $like: Op.like,
+    $notLike: Op.notLike,
+    $iLike: Op.iLike,
+    $notILike: Op.notILike,
+    $regexp: Op.regexp,
+    $notRegexp: Op.notRegexp,
+    $iRegexp: Op.iRegexp,
+    $notIRegexp: Op.notIRegexp,
+    $between: Op.between,
+    $notBetween: Op.notBetween,
+    $overlap: Op.overlap,
+    $contains: Op.contains,
+    $contained: Op.contained,
+    $adjacent: Op.adjacent,
+    $strictLeft: Op.strictLeft,
+    $strictRight: Op.strictRight,
+    $noExtendRight: Op.noExtendRight,
+    $noExtendLeft: Op.noExtendLeft,
+    $and: Op.and,
+    $or: Op.or,
+    $any: Op.any,
+    $all: Op.all,
+    $values: Op.values,
+    $col: Op.col
+};
+
+
 var mysql = require('mysql');
 var con = mysql.createConnection({
         host: "amedic-mysqldbserver.mysql.database.azure.com",
@@ -11,6 +50,7 @@ var con = mysql.createConnection({
 const db = new Sequelize('amedicdb', 'mysqldbuser@amedic-mysqldbserver', 'Grupp2122', {
     host: "amedic-mysqldbserver.mysql.database.azure.com",
     dialect: "mysql",
+    operatorsAliases,
     pool:{
         min:0,
         acquire: 30000,
