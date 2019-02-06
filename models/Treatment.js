b8f58a6b43f4c2039f9ad859602a40693388a5b6
luddelex <35630379+luddelex@users.noreply.github.com>
2019-02-06 10:48:23 +0100

    Update Treatment.js

--- models/Treatment.js
+++ models/Treatment.js
@@ -1,7 +1,10 @@
 /* jshint indent: 2 */
 
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Treatment', {
+const bcrypt = require('bcrypt');
+const DataTypes = require('sequelize/lib/data-types');
+const db = require('../connect.js');
+
+  var Treatment = db.define('Treatment', {
     ID: {
       type: DataTypes.INTEGER(10).UNSIGNED,
       allowNull: false,
@@ -25,6 +28,8 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: true
     }
   }, {
-    tableName: 'Treatment'
-  });
+    tableName: 'Treatment',
+    timestamps: false
+  })
 };
+module.exports = Treatment;
