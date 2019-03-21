857bf888704989ca77a6b9655dc3d77d8abadf68
folkelindell <fo6485ni-s@student.lu.se>
2019-03-21 17:17:35 +0100

    Lagt till ramverk för att visa success/error alerts under navbar

---
+++ scripts/feedbackAlert.js
@@ -0,0 +1,20 @@
+function showError(message) {
+    $('#alert_placeholder').append('<div class="alert alert-danger alert-dismissible fade in">' +
+        '<a href="#" class="close" data-dismiss="alert" aria-label="close">' +
+        '&times;' +
+        '</a>' +
+        '<strong>Error! </strong>'
+        + message +
+        '. </div>');
+}
+
+
+function showSuccess(message) {
+    $('#alert_placeholder').append('<div class="alert alert-success alert-dismissible fade in">' +
+        '<a href="#" class="close" data-dismiss="alert" aria-label="close">' +
+        '&times;' +
+        '</a>' +
+        '<strong>Success! </strong>'
+        + message +
+        '. </div>');
+}
